9eb1d4edf2d295c4ce0bd81aaec99bcaec80a0ee
Srigadgeter <sridhar.t@ezedox.com>
2020-01-25 20:32:48 +0530

    enabled& designed django admin for the profiles_api

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserProfile
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display=['email', 'name', 'is_active', 'is_superuser', 'is_staff']
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
