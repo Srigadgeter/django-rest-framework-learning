3d382b91fc057db16944ce2a821c3153ef28972d
Srigadgeter <sridhar.t@ezedox.com>
2020-01-25 22:15:24 +0530

    created serializer and implemented post call

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api.serializers import HelloSerializer
 
 
 class HelloApiView(APIView):
     '''Test API View'''
+
+    serializer_class = HelloSerializer
 
     def get(self, request, format=None):
         data = [
@@ -11,8 +16,22 @@ class HelloApiView(APIView):
             'API Example',
             'Test GET API Call'
         ]
-
         return Response({
             'message': 'This is GET api call',
             'data': data
         })
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}, You have successfully done the post api call'
+            return Response({
+                'message': message
+            })
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
