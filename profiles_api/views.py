3675a32810426c4c9dee7b56dc4b6593f0eb7a36
Srigadgeter <sridhar.t@ezedox.com>
2020-01-25 21:45:23 +0530

    Added HelloApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    '''Test API View'''
+
+    def get(self, request, format=None):
+        data = [
+            'Django Rest Framework',
+            'API Example',
+            'Test GET API Call'
+        ]
+
+        return Response({
+            'message': 'This is GET api call',
+            'data': data
+        })
