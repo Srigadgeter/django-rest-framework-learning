1e50fe0e31e8801470b6fe081b06b55a3438fa10
Srigadgeter <sridhar.t@ezedox.com>
2020-01-26 11:00:19 +0530

    changes in put, patch, delete functions

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -36,19 +36,19 @@ class HelloApiView(APIView):
                 status=status.HTTP_400_BAD_REQUEST
             )
 
-    def put(self, request):
+    def put(self, request, pk=None):
         '''Handles updating an object'''
         return Response({
             'method': 'PUT'
         })
 
-    def patch(self, request):
+    def patch(self, request, pk=None):
         '''Handles a partial update of an object'''
         return Response({
             'method': 'PATCH'
         })
 
-    def delete(self, request):
+    def delete(self, request, pk=None):
         '''Delete an object'''
         return Response({
             'method': 'DELETE'
