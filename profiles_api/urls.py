89252e0f741a2ae896fd79084e2fd25e6ddf670a
Srigadgeter <sridhar.t@ezedox.com>
2020-01-26 17:32:01 +0530

    auth token added

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import HelloApiView, HelloViewSet, UserProfileViewSet
+from .views import HelloApiView, HelloViewSet, UserProfileViewSet, UserLoginApiView
 
 router = DefaultRouter()
 router.register('hello-viewset', HelloViewSet, base_name='hello-viewset')
@@ -9,5 +9,6 @@ router.register('profile', UserProfileViewSet)
 
 urlpatterns = [
     path('hello/', HelloApiView.as_view()),
+    path('login/', UserLoginApiView.as_view()),
     path('', include(router.urls))
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,8 @@ from rest_framework import status
 from rest_framework.viewsets import ViewSet, ModelViewSet
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.filters import SearchFilter
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from .serializers import HelloSerializer, UserProfileSerializer
 from .models import UserProfile
@@ -125,4 +127,10 @@ class UserProfileViewSet(ModelViewSet):
     permission_classes = (UpdateOwnProfile,)
     filter_backends = (SearchFilter,)
     search_fields = ('name', 'email')
+
+
+class UserLoginApiView(ObtainAuthToken):
+    '''Handle creating user authentication tokens'''
+
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
