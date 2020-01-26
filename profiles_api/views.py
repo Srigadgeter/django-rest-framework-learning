5669e1631ebad8d05d89a2cb5713167070e1fa2e
Srigadgeter <sridhar.t@ezedox.com>
2020-01-26 14:34:21 +0530

    added permission for the profile's apis

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,9 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.viewsets import ViewSet, ModelViewSet
+from rest_framework.authentication import TokenAuthentication
 
 from .serializers import HelloSerializer, UserProfileSerializer
 from .models import UserProfile
+from .permissions import UpdateOwnProfile
 
 
 class HelloApiView(APIView):
@@ -118,4 +120,6 @@ class UserProfileViewSet(ModelViewSet):
 
     serializer_class = UserProfileSerializer
     queryset = UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (UpdateOwnProfile,)
 
