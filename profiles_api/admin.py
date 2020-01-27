24854d5672c50bab03ce1ddad8b1006ebc688cab
Srigadgeter <sridhar.t@ezedox.com>
2020-01-27 15:29:38 +0530

    admin page improved for the ProfileFeedItem model

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from .models import UserProfile
+from .models import UserProfile, ProfileFeedItem
 
 
 class UserProfileAdmin(admin.ModelAdmin):
     list_display=['email', 'name', 'is_active', 'is_superuser', 'is_staff']
 
 
+class ProfileFeedItemAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user_profile', 'status_text']
+
+
 admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(ProfileFeedItem, ProfileFeedItemAdmin)
