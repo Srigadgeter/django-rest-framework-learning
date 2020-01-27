5e44f09030821a554a1e651885ff20d0c3222afb
Srigadgeter <sridhar.t@ezedox.com>
2020-01-27 15:25:06 +0530

    permission page typo mistakes resolved

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -6,11 +6,10 @@ class UpdateOwnProfile(permissions.BasePermission):
     Allow user to edit their own profile
     '''
     
-    def has_object_permisssion(self, request, view, obj):
+    def has_object_permission(self, request, view, obj):
         '''Check user is trying to edit their own profile'''
         if request.method in permissions.SAFE_METHODS:
             return True
-        
         return obj.id == request.user.id
 
 
@@ -19,7 +18,7 @@ class UpdateOwnStatus(permissions.BasePermission):
     Allow users to update their own status
     '''
 
-    def has_object_permisssion(self, request, view, obj):
+    def has_object_permission(self, request, view, obj):
         '''Check the user is trying to update their own status'''
         if request.method in permissions.SAFE_METHODS:
             return True
